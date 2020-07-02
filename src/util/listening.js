4811757f9495d6791c573b0e937c7b00888a8baf
Riday <ridayshah2001@gmail.com>
2020-07-02 18:28:50 +0530

    Keep the app awake
    Call wakeUp() function

--- src/util/listening.js
+++ src/util/listening.js
@@ -1,4 +1,7 @@
 const debug = require('debug')('ht-site:server');
+const { wakeUp } = require('./wakeUp');
+
 exports.listening = () => {
+	wakeUp();
 	debug(`Listening on port ${process.env.PORT ?? 8080}.`);
 };
