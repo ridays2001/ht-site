65ca871a53d13ecc613b4420a2d43d39307d9ca2
Riday <ridayshah2001@gmail.com>
2020-07-03 17:58:34 +0530

    Fix: Temp
    Added temporary mediocre cookie system for logins.

--- src/users.js
+++ src/users.js
@@ -2,12 +2,26 @@ const express = require('express');
 const router = express.Router();
 const { firestore: db } = require('./util/db');
 
+router.get('/', (req, res) => {
+	const id = req.cookies.id;
+	const username = req.cookies.username;
+	const data = db.collection('users').doc(username).get()
+		.then(snap => snap.data()?.data);
+	if (data?.id === id) return res.redirect(301, `/users/${username}`);
+	return res.redirect(303, '/login');
+});
+
 router.get('/:user', (req, res) => {
 	if (!req.params.user) return res.status(403).redirect('/');
-
-	db.collection('users').doc(req.params.user).get()
-		.then(snap => console.log(snap.data()));
-	return res.send('Checking...');
+	const id = req.cookies.id;
+	const username = req.cookies.username;
+	if (id && username) {
+		const data = db.collection('users').doc(username).get()
+			.then(snap => snap.data()?.data);
+		if (data?.id === id) return res.send('✅ Logged In Successfully.\nThere is no data here though.');
+	}
+	console.log('Redirected to login.');
+	res.redirect(303, '/login');
 });
 
 module.exports = router;
