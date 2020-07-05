abda532e9fe44e6661ef15d10ac913e8cb2aa32c
Riday <ridayshah2001@gmail.com>
2020-07-05 21:19:22 +0530

    Fix: content
    Redirect /marks and /syllabus to the link provided in the database.

--- src/users.js
+++ src/users.js
@@ -2,14 +2,13 @@ const express = require('express');
 const router = express.Router();
 const { firestore: db } = require('./util/db');
 const { authenticate } = require('./util/authenticate');
-const { userData, assignments, attendance, syllabus, notes } = require('./util/userData');
+const { userData, assignments, attendance, syllabus, notes, marks } = require('./util/userData');
 const moment = require('moment');
 require('moment-duration-format');
 
 router.get('/', (req, res) => {
 	const auth = authenticate(undefined, req.cookies);
 	if (auth) return res.redirect(`/users/${req.cookies.username}`);
-	console.log('Redirected back to login page.');
 	return res.redirect(403, '/login');
 });
 
@@ -17,14 +16,10 @@ router.get('/:user', async (req, res) => {
 	const user = req.params.user;
 	if (!user) return res.redirect(403, '/login');
 	const auth = await authenticate(undefined, req.cookies);
-	if (!auth) {
-		console.log('Authentication failed. Redirected to login.');
-		return res.redirect('/login');
-	}
+	if (!auth) return res.redirect(403, '/login');
 
 	const data = await db.collection('users').doc(user).get()
 		.then(snap => snap.data()?.data);
-
 	return res.render('users/user-home', {
 		name: data.name,
 		username: user
@@ -33,12 +28,39 @@ router.get('/:user', async (req, res) => {
 
 router.get('/:user/assignments', async (req, res) => {
 	const user = req.params.user;
-	return res.render('users/assignments', await userData(user));
+	const auth = await authenticate(undefined, req.cookies);
+	if (!auth) return res.redirect(403, '/login');
+	return res.render('users/assignments', await assignments(user));
+});
+
+router.get('/:user/notes', async (req, res) => {
+	const user = req.params.user;
+	const auth = await authenticate(undefined, req.cookies);
+	if (!auth) return res.redirect(403, '/login');
+	return res.render('users/notes', await notes(user));
 });
 
 router.get('/:user/attendance', async (req, res) => {
 	const user = req.params.user;
+	const auth = await authenticate(undefined, req.cookies);
+	if (!auth) return res.redirect(403, '/login');
 	return res.render('users/attendance', await attendance(user));
+});
+
+router.get('/:user/marks', async (req, res) => {
+	const user = req.params.user;
+	const auth = await authenticate(undefined, req.cookies);
+	if (!auth) return res.redirect(403, '/login');
+	const { link } = await marks(user);
+	return res.redirect(link);
+});
+
+router.get('/:user/syllabus', async (req, res) => {
+	const user = req.params.user;
+	const auth = await authenticate(undefined, req.cookies);
+	if (!auth) return res.redirect(403, '/login');
+	const { link } = await syllabus(user);
+	return res.redirect(link);
 });
 
 module.exports = router;
