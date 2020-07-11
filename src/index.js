89782f5e4b68dd19388c6cec5965e1b297c956de
Riday <ridayshah2001@gmail.com>
2020-07-11 10:01:33 +0530

    Fix: aesthetics
    - Rearranged code into sections.
    - Added comments.
    - Added auth router.
    - Configured sentry.

--- src/index.js
+++ src/index.js
@@ -9,20 +9,33 @@ const { flash } = require('express-flash-message');
 const logger = require('morgan');
 const http = require('http');
 const { FirestoreStore } = require('@google-cloud/connect-firestore');
+const sentry = require('@sentry/node');
+
+// Import all modules.
 const { authenticate } = require('./util/authenticate');
 const { firestore: db } = require('./util/db');
 const { listening: onListening, err: onError } = require('./util/listeners');
 
+// Initialize an express app.
 const express = require('express');
 const app = express();
 
-app.set('port', process.env.PORT ?? 8080); // Use port 80 for hosting.
+sentry.init({ dsn: process.env.SENTRY });
 
+app.set('port', process.env.PORT ?? 80); // Use port 80 for hosting.
+
+// Views section.
 app.set('views', path.join(__dirname, './views')); // Configure views folder to use for view engine.
 app.set('view engine', 'pug'); // Set view engine to pug.
 
+// Middleware section.
+app.use(sentry.Handlers.requestHandler()); // Use sentry.
 app.use(express.json()); // Parse incoming json requests.
 app.use(express.urlencoded({ extended: true })); // Parse the incoming requests to urlencoded payloads
+app.use(logger('dev')); // Use morgan logger to log http requests.
+app.use(express.static(path.join(__dirname, 'public'))); // Configure the public folder into the sitemap.
+
+// Sessions and cookies section.
 app.use(session({
 	name: 'welcome-to-hell',
 	secret: process.env.SECRET,
@@ -30,20 +43,16 @@ app.use(session({
 		sameSite: 'lax',
 		secure: true,
 		httpOnly: true,
-		path: '/',
 		maxAge: 3600000
 	},
 	resave: false,
 	saveUninitialized: true,
-	store: new FirestoreStore({
-		dataset: db
-	  })
+	store: new FirestoreStore({ dataset: db })
 })); // Use sessions in the app.
 app.use(cookieParser(process.env.SECRET)); // Use cookies in the app.
-app.use(flash({ sessionKeyName: 'notifications' }));
-app.use(logger('dev')); // Use morgan logger to log http requests.
-app.use(express.static(path.join(__dirname, 'public'))); // Configure the public folder into the sitemap.
+app.use(flash({ sessionKeyName: 'notifications' })); // Use flash messages.
 
+// Handle defined routes.
 app.get('/', async (req, res) => {
 	let loginButton = undefined;
 	const auth = await authenticate(undefined, req.cookies);
@@ -55,75 +64,7 @@ app.get('/', async (req, res) => {
 	else device = 'desktop';
 	res.render('index', { view: device, loginButton });
 });
-
-app.use('/users', require('./users'));
-
-app.get('/login', async (req, res) => {
-	// Check if the user already has saved login on their device.
-	if (req.cookies.id || req.cookies.username) {
-		const auth = await authenticate(undefined, req.cookies);
-		if (auth) return res.redirect(`/users/${req.cookies.username}`);
-		res.cookie('id', undefined, { maxAge: 100 });
-		res.cookie('username', undefined, { maxAge: 100 });
-		console.log('Cookies have been deleted.');
-		return res.redirect('/login');
-	}
-	const viewport = req.headers['user-agent'].toLowerCase();
-	let device = undefined;
-	if (viewport.includes('android') || viewport.includes('iphone') || viewport.includes('ipad')) device = 'mobile';
-	else device = 'desktop';
-	res.render('login', {
-		view: device,
-		err: await req.consumeFlash('error'),
-		success: await req.consumeFlash('success')
-	});
-});
-
-app.post('/login', async (req, res) => {
-	const loginData = {
-		username: req.body.username,
-		password: req.body.pass,
-		id: req.session.id
-	};
-	const auth = await authenticate(loginData, undefined);
-	if (auth) {
-		res.cookie('username', loginData.username, {
-			maxAge: 3.154e+10,
-			sameSite: 'lax',
-			secure: true,
-			path: '/',
-			httpOnly: true
-		});
-		res.cookie('id', loginData.id, {
-			maxAge: 3.154e+10,
-			sameSite: 'lax',
-			secure: true,
-			path: '/',
-			httpOnly: true
-		});
-		console.log('Redirected to users page.');
-		return res.redirect(`/users/${loginData.username}`);
-	}
-
-	// Delete old cookies after 100 ms.
-	if (req.cookies.username) res.cookie('username', undefined, { maxAge: 100 });
-	if (req.cookies.id) res.cookie('id', undefined, { maxAge: 100 });
-	await req.flash('error', '❌ Incorrect Login Credentials.');
-	return res.redirect('/login');
-});
-
-app.get('/logout', async (req, res) => {
-	const { username, id } = req.cookies;
-	if (username) res.cookie('username', undefined, { maxAge: 100 });
-	if (id) res.cookie('id', undefined, { maxAge: 100 });
-	const data = await db.collection('users').doc(username).get()
-		.then(snap => snap.data()?.data);
-	data.id.splice(data.id.indexOf(id), 1);
-	await db.collection('users').doc(username).set({ data }, { merge: true });
-	await req.flash('success', '✅ Logged out successfully.');
-	return res.redirect('/login');
-});
-
+app.get('/home', (_req, res) => res.render('home'));
 app.get('/contact', (req, res) => {
 	const viewport = req.headers['user-agent'].toLowerCase();
 	let device = undefined;
@@ -132,12 +73,21 @@ app.get('/contact', (req, res) => {
 	res.render('contact', { view: device });
 });
 
-app.get('/tos', (_req, res) => res.redirect(301, '/terms'));
-app.get('/tnc', (_req, res) => res.redirect(301, '/terms'));
+// Legal section.
 app.get('/terms', (_req, res) => res.render('legal/terms'));
 app.get('/privacy', (_req, res) => res.render('legal/privacy'));
 app.get('/disclaimer', (_req, res) => res.render('legal/disclaimer'));
 
+// Redirect common paths for terms and conditions.
+app.get('/tos', (_req, res) => res.redirect(301, '/terms'));
+app.get('/tnc', (_req, res) => res.redirect(301, '/terms'));
+
+// Use router modules.
+app.use('/users', require('./routers/users'));
+app.use('/auth', require('./routers/auth'));
+
+// Error handling section.
+app.use(sentry.Handlers.errorHandler());
 app.use((_req, res) => {
 	res.status(404).redirect('/');
 });
@@ -153,7 +103,8 @@ app.use((err, req, res) => {
 	res.render('error', { error: err });
 });
 
+// Node server section.
 const server = http.createServer(app);
-server.listen(process.env.PORT ?? 8080);
+server.listen(process.env.PORT ?? 80);
 server.on('listening', onListening);
 server.on('error', onError);
