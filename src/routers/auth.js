933aa43c38ab3d37c6fc3d77d9ed4b12af01d39e
Riday <ridayshah2001@gmail.com>
2020-07-11 10:02:36 +0530

    Added auth router
    Split multiple routes into modules.

---
+++ src/routers/auth.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const router = express.Router();
+const { authenticate } = require('../util/authenticate');
+const { firestore: db } = require('../util/db');
+
+router.get('/login', async (req, res) => {
+	// Check if the user already has a saved login on their device.
+	if (req.cookies.id || req.cookies.username) {
+		const auth = await authenticate(undefined, req.cookies);
+		if (auth) return res.redirect(`/users/${req.cookies.username}`);
+
+		// Delete cookies in case of a failed authentication.
+		res.cookie('id', undefined, { maxAge: 100 });
+		res.cookie('username', undefined, { maxAge: 100 });
+		console.log('[AUTH FAILED] Cookies have been deleted.');
+		return res.redirect('/login');
+	}
+	const viewport = req.headers['user-agent'].toLowerCase();
+	let device = undefined;
+	if (viewport.includes('android') || viewport.includes('iphone') || viewport.includes('ipad')) device = 'mobile';
+	else device = 'desktop';
+	res.render('login', {
+		view: device,
+		err: await req.consumeFlash('error'),
+		success: await req.consumeFlash('success')
+	});
+});
+
+router.post('/login', async (req, res) => {
+	const auth = await authenticate({
+		username: req.body.username,
+		password: req.body.password,
+		id: req.session.id
+	}, undefined);
+
+	if (auth) {
+		res.cookie('username', req.body.username, {
+			maxAge: 3.154e+10,
+			sameSite: 'lax',
+			httpOnly: true,
+			secure: true
+		});
+		res.cookie('id', req.session.id, {
+			maxAge: 3.154e+10,
+			sameSite: 'lax',
+			httpOnly: true,
+			secure: true
+		});
+		console.log('[AUTH SUCCESS] Cookies have been created. Redirecting to users page...');
+		return res.redirect(`/users/${req.body.username}`);
+	}
+
+	// Delete old cookies, if present.
+	if (req.cookies.username) res.cookie('username', undefined, { maxAge: 100 });
+	if (req.cookies.id) res.cookie('id', undefined, { maxAge: 100 });
+
+	await req.flash('error', '❌ Incorrect Login Credentials.');
+	return res.redirect('/login');
+});
+
+router.get('/logout', async (req, res) => {
+	const { username, id } = req.cookies;
+	if (username) res.cookie('username', undefined, { maxAge: 100 });
+	if (id) res.cookie('id', undefined, { maxAge: 100 });
+	const data = await db.collection('users').doc(username).get()
+		.then(snap => snap.data()?.data);
+	data.id.splice(data.id.indexOf(id), 1);
+	await db.collection('users').doc(username).set({ data }, { merge: true });
+	await req.flash('success', '✅ Logged out successfully.');
+	return res.redirect('/login');
+});
+
+module.exports = router;
