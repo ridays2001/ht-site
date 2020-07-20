825996a3e215754ce19272866b77c253722ad7c2
Riday <ridayshah2001@gmail.com>
2020-07-20 09:30:15 +0530

    Fix: removed apm

--- src/util/sentry.js
+++ src/util/sentry.js
@@ -3,16 +3,12 @@
  * Sentry is an error tracking service which helps in improving the app performance.
  */
 const sentry = require('@sentry/node');
-const apm = require('@sentry/apm');
-
-const app = require('../app');
 
 sentry.init({
 	dsn: process.env.SENTRY,
 	environment: process.env.NODE_ENV,
 	integrations: [
-		new sentry.Integrations.Http({ tracing: true }),
-		new apm.Integrations.Express({ app })
+		new sentry.Integrations.Http({ tracing: true })
 	],
 	tracesSampleRate: 1.0
 });
