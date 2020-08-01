4e2e674c32ae7a57e199dad13bd89347f58b4496
Riday <ridayshah2001@gmail.com>
2020-08-01 09:45:23 +0530

    Fix: login information
    - Store devices and login time.
    - Moved saved ids to saved object.

--- src/util/authenticate.js
+++ src/util/authenticate.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 const { firestore: db } = require('./db');
 
 module.exports = async (loginData, cookieData) => {
@@ -6,12 +7,14 @@ module.exports = async (loginData, cookieData) => {
 	const username = loginData?.username;
 	const password = loginData?.password;
 	const id = loginData?.id;
+	const userAgent = loginData?.ua; // User Agent.
 
 	// Check if the saved cookie matches the credentials in the database.
 	if (savedUsername && savedID) {
 		const data = await db.collection('users').doc(savedUsername).get()
 			.then(snap => snap.data()?.data);
-		if (data?.id?.includes(savedID)) return { auth: true, rank: data.rank };
+		const ids = data?.saved?.map(s => s?.id) ?? [];
+		if (ids.includes(savedID)) return { auth: true, rank: data.rank };
 	}
 
 	// Check if the login credentials entered match with the database.
@@ -19,8 +22,12 @@ module.exports = async (loginData, cookieData) => {
 		const data = await db.collection('users').doc(username).get()
 			.then(snap => snap.data()?.data);
 		if (data?.password === password) {
-			if (!data?.id?.length) data.id = [];
-			data.id.push(id);
+			if (!data?.saved?.length) data.saved = [];
+			data.saved.push({
+				id,
+				userAgent,
+				date: moment.tz(new Date(), 'Asia/Kolkata').format('Do MMM YY, kk:mm - dddd')
+			});
 			await db.collection('users').doc(username).set({ data }, { merge: true });
 			return { auth: true, rank: data.rank };
 		}
